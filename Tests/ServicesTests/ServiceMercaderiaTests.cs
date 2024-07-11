06a49adc3cc5d4e71790ab00d853e0ea23c4ec4b
NicoDVKa <herlannicolas6@gmail.com>
2024-07-11 11:26:05 -0300

    add  GetAllMercaderias Test

---
+++ Tests/ServicesTests/ServiceMercaderiaTests.cs
@@ -0,0 +1,91 @@
+﻿
+using Application.Interfaces;
+using Application.Models.Response;
+using Application.UseCases;
+using Domain.Entities;
+using FluentAssertions;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Tests.ServicesTests
+{
+    public class ServiceMercaderiaTests
+    {
+
+        [Fact]
+        public async Task GetAllMercaderia_ShouldReturnListOfMercaderiaResponse()
+        {
+            // Arrange
+            var mockQuery = new Mock<IQueryMercaderia>();
+            var mockCommand = new Mock<ICommandMercaderia>();
+
+            IList<Mercaderia> listOfMercaderia = new List<Mercaderia>()
+            {
+                new Mercaderia { MercaderiaId = 1,
+                                 Nombre = "Producto 1",
+                                 Imagen = "x",
+                                 Preparacion = "x",
+                                 Precio = 100,
+                                 Ingredientes = "x",
+                                 TipoMercaderia = new TipoMercaderia
+                                 {
+                                     TipoMercaderiaId = 1,
+                                     Descripcion = "1",
+                                 }
+                },
+                new Mercaderia { MercaderiaId = 2,
+                                 Nombre = "Producto 2",
+                                 Imagen = "a",
+                                 Preparacion = "a",
+                                 Precio = 50,
+                                 Ingredientes = "a",
+                                 TipoMercaderia = new TipoMercaderia
+                                 {
+                                     TipoMercaderiaId = 2,
+                                     Descripcion = "a",
+                                 }
+                }
+            };
+
+            IList<MercaderiaResponse> expectedMercaderiaResponse = new List<MercaderiaResponse>()
+            {
+                new MercaderiaResponse { Id = 1,
+                                         Nombre = "Producto 1",
+                                         Imagen = "x",
+                                         Preparacion = "x",
+                                         Precio = 100,
+                                         Ingredientes = "x",
+                                         Tipo = new TipoMercaderiaResponse
+                                         {
+                                             Id = 1,
+                                             Descripcion = "1",
+                                         }
+                },
+                new MercaderiaResponse { Id = 2,
+                                         Nombre = "Producto 2",
+                                         Imagen = "a",
+                                         Preparacion = "a",
+                                         Precio = 50,
+                                         Ingredientes = "a",
+                                         Tipo = new TipoMercaderiaResponse
+                                         {
+                                             Id = 2,
+                                             Descripcion = "a",
+                                         }
+                }
+            };
+
+            mockQuery.Setup(q => q.GetAllMercaderias()).Returns(Task.FromResult(listOfMercaderia));
+
+            var service = new ServiceMercaderia(mockQuery.Object, mockCommand.Object);
+
+            // Act
+            var result = await service.GetAllMercaderia();
+
+            // Assert
+            Assert.Equivalent(expectedMercaderiaResponse, result);     
+        }
+    }
+}
