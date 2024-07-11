d30c732b5770471b910c545995dad4f250d7b616
NicoDVKa <herlannicolas6@gmail.com>
2024-07-11 20:59:54 -0300

    add service tipo mercaderia tests

--- Tests/ServicesTests/ServiceTipoMercaderiaTests.cs
+++ Tests/ServicesTests/ServiceTipoMercaderiaTests.cs
@@ -1,8 +1,60 @@
 ﻿
 
+using Application.Interfaces;
+using Application.Models.Response;
+using Application.UseCases;
+using Domain.Entities;
+using FluentAssertions;
+using Moq;
+
 namespace Tests.ServicesTests
 {
     public class ServiceTipoMercaderiaTests
     {
+        [Fact]
+        public async Task GetTipoMercaderiaById_ShouldReturnTipoMercaderia()
+        {
+            // Arrange
+            var mockCommand = new Mock<ICommandTipoMercaderia>();
+            var mockQuery = new Mock<IQueryTipoMercaderia>();
+
+            TipoMercaderia tipoMercaderia = new TipoMercaderia()
+            {
+                TipoMercaderiaId = 1,
+                Descripcion = "Entrada"
+            };
+
+            TipoMercaderiaResponse expectedTipoMercaderia = new TipoMercaderiaResponse()
+            {
+                Id = 1,
+                Descripcion = "Entrada"
+            };
+
+            mockQuery.Setup(q => q.GetTipoMercaderiaById(It.IsAny<int>())).ReturnsAsync(tipoMercaderia);
+
+            var service = new ServiceTipoMercaderia(mockQuery.Object, mockCommand.Object);
+
+            // Act
+            var result = await service.GetTipoMercaderiaById(It.IsAny<int>());
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedTipoMercaderia);
+        }
+
+        [Fact]
+        public async Task GetTipoMercaderiaById_ShouldReturnNull()
+        {
+            // Arrange
+            var mockCommand = new Mock<ICommandTipoMercaderia>();
+            var mockQuery = new Mock<IQueryTipoMercaderia>();
+
+            var service = new ServiceTipoMercaderia(mockQuery.Object, mockCommand.Object);
+
+            // Act
+            var result = await service.GetTipoMercaderiaById(It.IsAny<int>());
+
+            // Assert
+            result.Should().BeNull();
+        }
     }
 }
