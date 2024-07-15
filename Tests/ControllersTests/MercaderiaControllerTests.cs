e8ca8357152d55395ef266d233003e9923199ad4
NicoDVKa <herlannicolas6@gmail.com>
2024-07-15 09:56:43 -0300

    Test:  add Create Mercaderia tests

--- Tests/ControllersTests/MercaderiaControllerTests.cs
+++ Tests/ControllersTests/MercaderiaControllerTests.cs
@@ -1,7 +1,9 @@
 ﻿
 
 using Application.Interfaces;
+using Application.Models.Request;
 using Application.Models.Response;
+using Azure.Core;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Update;
@@ -145,53 +147,217 @@ namespace Tests.ControllersTests
         [Fact]
         public async Task CreateMercaderia_MercaderiaIsValid_ShouldReturnMercaderiaResponseWithStatusCode201()
         {
-            // TODO
-
             // Arrange
+            var mockServiceMercaderia = new Mock<IServiceMercaderia>();
+            var mockServiceTipoMercaderia = new Mock<IServiceTipoMercaderia>();
+            var mockServiceComandaMercaderia = new Mock<IServiceComandaMercaderia>();
+            var mockServiceValidateMercaderia = new Mock<IServiceValidateMercaderia>();
+
+            var expectedMercaderiaResponse = new MercaderiaResponse()
+            {
+                Id = 1,
+                Nombre = "a",
+                Precio = 1,
+                Imagen = "a",
+                Ingredientes = "a",
+                Preparacion = "a",
+                Tipo = new TipoMercaderiaResponse()
+                {
+                    Id = 1,
+                    Descripcion = "a",
+                }
+            };
+
+            var mercaderiaRequest = new MercaderiaRequest()
+            {
+                Nombre = "a",
+                Precio = 1,
+                Imagen = "a",
+                Ingredientes = "a",
+                Preparacion = "a",
+                Tipo = 1
+            };
+
+            var tipoMercaderiaResponse = new TipoMercaderiaResponse()
+            {
+                Id = 1,
+                Descripcion = "a",
+            };
+
+            mockServiceMercaderia.Setup(m => m.CreateMercaderia(It.IsAny<MercaderiaRequest>()))
+                                 .ReturnsAsync(expectedMercaderiaResponse);
+            
+            mockServiceValidateMercaderia.Setup(m => m.MercaderiaIsValid(It.IsAny<MercaderiaRequest>(), false))
+                                         .ReturnsAsync(true);
+
+            mockServiceTipoMercaderia.Setup(m => m.GetTipoMercaderiaById(It.IsAny<int>()))
+                                     .ReturnsAsync(tipoMercaderiaResponse);
+
+            var controller = new MercaderiaController(mockServiceMercaderia.Object,
+                                                      mockServiceTipoMercaderia.Object,
+                                                      mockServiceComandaMercaderia.Object,
+                                                      mockServiceValidateMercaderia.Object);
 
             //Act
+            var result = await controller.CreateMercaderia(mercaderiaRequest) as JsonResult;
+
 
             // Assert
-            Assert.Fail();
+            result.StatusCode.Should().Be(201);
+            result.Value.Should().BeEquivalentTo(expectedMercaderiaResponse);
         }
 
         [Fact]
         public async Task CreateMercaderia_MercaderiaIsNotValid_ShouldReturnBadRequestWithStatusCode400()
         {
-            // TODO
-
             // Arrange
+            var mockServiceMercaderia = new Mock<IServiceMercaderia>();
+            var mockServiceTipoMercaderia = new Mock<IServiceTipoMercaderia>();
+            var mockServiceComandaMercaderia = new Mock<IServiceComandaMercaderia>();
+            var mockServiceValidateMercaderia = new Mock<IServiceValidateMercaderia>();
+
+            var mercaderiaInvalidRequest = new MercaderiaRequest()
+            {
+                Nombre = It.IsAny<string>(),
+                Precio = It.IsAny<double>(),
+                Imagen = It.IsAny<string>(),
+                Ingredientes = It.IsAny<string>(),
+                Preparacion = It.IsAny<string>(),
+                Tipo = It.IsAny<int>()
+            };
+
+            var expectedResponse = new BadRequest()
+            {
+                Message = "Algun error en uno de los campos"
+            };
+
+            mockServiceValidateMercaderia.Setup(m => m.MercaderiaIsValid(It.IsAny<MercaderiaRequest>(), false))
+                                         .ReturnsAsync(false);
+
+            mockServiceValidateMercaderia.Setup(m => m.GetError())
+                                    .Returns(expectedResponse.Message);
+
+            var controller = new MercaderiaController(mockServiceMercaderia.Object,
+                                                      mockServiceTipoMercaderia.Object,
+                                                      mockServiceComandaMercaderia.Object,
+                                                      mockServiceValidateMercaderia.Object);
 
             //Act
+            var result = await controller.CreateMercaderia(mercaderiaInvalidRequest) as JsonResult;
+
 
             // Assert
-            Assert.Fail();
+            result.StatusCode.Should().Be(400);
+            result.Value.Should().BeEquivalentTo(expectedResponse);
         }
 
         [Fact]
         public async Task CreateMercaderia_TipoMercaderiaDoesNotExist_ShouldReturnBadRequestWithStatusCode400()
         {
-            // TODO
-
             // Arrange
+            var mockServiceMercaderia = new Mock<IServiceMercaderia>();
+            var mockServiceTipoMercaderia = new Mock<IServiceTipoMercaderia>();
+            var mockServiceComandaMercaderia = new Mock<IServiceComandaMercaderia>();
+            var mockServiceValidateMercaderia = new Mock<IServiceValidateMercaderia>();
+
+            var mercaderiaRequest = new MercaderiaRequest()
+            {
+                Nombre = "a",
+                Precio = 1,
+                Imagen = "a",
+                Ingredientes = "a",
+                Preparacion = "a",
+                Tipo = 1
+            };
+
+            var expectedResponse = new BadRequest()
+            {
+                Message = $"No existe un tipo de mercaderia con el ID {mercaderiaRequest.Tipo}"
+            };
+
+            mockServiceValidateMercaderia.Setup(m => m.MercaderiaIsValid(It.IsAny<MercaderiaRequest>(), false))
+                                         .ReturnsAsync(true);
+
+            var controller = new MercaderiaController(mockServiceMercaderia.Object,
+                                                      mockServiceTipoMercaderia.Object,
+                                                      mockServiceComandaMercaderia.Object,
+                                                      mockServiceValidateMercaderia.Object);
 
             //Act
+            var result = await controller.CreateMercaderia(mercaderiaRequest) as JsonResult;
+
 
             // Assert
-            Assert.Fail();
+            result.StatusCode.Should().Be(400);
+            result.Value.Should().BeEquivalentTo(expectedResponse);
         }
 
         [Fact]
         public async Task CreateMercaderia_NombreMercaderiaDoesExist_ShouldReturnBadRequestWithStatusCode409()
         {
-            // TODO
-
             // Arrange
+            var mockServiceMercaderia = new Mock<IServiceMercaderia>();
+            var mockServiceTipoMercaderia = new Mock<IServiceTipoMercaderia>();
+            var mockServiceComandaMercaderia = new Mock<IServiceComandaMercaderia>();
+            var mockServiceValidateMercaderia = new Mock<IServiceValidateMercaderia>();
+
+            var mercaderiaResponse = new MercaderiaResponse()
+            {
+                Id = 1,
+                Nombre = "a",
+                Precio = 1,
+                Imagen = "a",
+                Ingredientes = "a",
+                Preparacion = "a",
+                Tipo = new TipoMercaderiaResponse()
+                {
+                    Id = 1,
+                    Descripcion = "a",
+                }
+            };
+
+            var mercaderiaRequest = new MercaderiaRequest()
+            {
+                Nombre = "a",
+                Precio = 1,
+                Imagen = "a",
+                Ingredientes = "a",
+                Preparacion = "a",
+                Tipo = 1
+            };
+
+            var tipoMercaderiaResponse = new TipoMercaderiaResponse()
+            {
+                Id = 1,
+                Descripcion = "a",
+            };
+
+            var expectedResponse = new BadRequest()
+            {
+               Message = "Ya existe una mercaderia con ese nombre"
+            };
+
+            mockServiceValidateMercaderia.Setup(m => m.MercaderiaIsValid(It.IsAny<MercaderiaRequest>(), false))
+                                         .ReturnsAsync(true);
+
+            mockServiceTipoMercaderia.Setup(m => m.GetTipoMercaderiaById(It.IsAny<int>()))
+                                     .ReturnsAsync(tipoMercaderiaResponse);
+
+            mockServiceMercaderia.Setup(m => m.GetMercaderiaByName(It.IsAny<string>()))
+                                 .ReturnsAsync(mercaderiaResponse);
+
+            var controller = new MercaderiaController(mockServiceMercaderia.Object,
+                                                      mockServiceTipoMercaderia.Object,
+                                                      mockServiceComandaMercaderia.Object,
+                                                      mockServiceValidateMercaderia.Object);
 
             //Act
+            var result = await controller.CreateMercaderia(mercaderiaRequest) as JsonResult;
+
 
             // Assert
-            Assert.Fail();
+            result.StatusCode.Should().Be(409);
+            result.Value.Should().BeEquivalentTo(expectedResponse);
         }
 
         [Theory]
