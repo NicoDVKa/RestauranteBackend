ec4ab098ea0371937d3878b76172a504fd19fb7a
NicoDVKa <herlannicolas6@gmail.com>
2024-07-18 20:31:27 -0300

    Test: add GetComandaById tests

--- Tests/QueriesTests/QueryComandaTests.cs
+++ Tests/QueriesTests/QueryComandaTests.cs
@@ -107,6 +107,148 @@ namespace Tests.QueriesTests
             comands.Should().BeEmpty();
         }
 
+        [Fact]
+        public async Task GetComandaById_ShouldReturnComanda()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+            using var context = new AppDbContext(options);
 
+            context.Database.EnsureDeleted();
+
+            var tipoMercaderia = new TipoMercaderia
+            {
+                Descripcion = "Entrada"
+            };
+            var mercaderia = new Mercaderia
+            {
+                Nombre = "Deditos de queso",
+                Imagen = "a",
+                Precio = 100,
+                Ingredientes = "a",
+                Preparacion = "a",
+                TipoMercaderiaId = 1
+            };
+            var formaEntrega = new FormaEntrega
+            {
+                Descripcion = "Salon"
+            };
+
+            context.TipoMercaderia.Add(tipoMercaderia);
+            context.Mercaderia.Add(mercaderia);
+            context.FormaEntrega.Add(formaEntrega);
+            context.SaveChanges();
+
+            var comanda1 = new Comanda
+            {
+                ComandaId = Guid.NewGuid(),
+                PrecioTotal = 100,
+                Fecha = DateTime.Now,
+                FormaEntregaId = formaEntrega.FormaEntregaId,
+                ComandaMercaderias = new List<ComandaMercaderia>
+                {
+                    new ComandaMercaderia { MercaderiaId = mercaderia.MercaderiaId }
+                }
+            };
+            var comanda2 = new Comanda
+            {
+                ComandaId = Guid.NewGuid(),
+                PrecioTotal = 200,
+                Fecha = DateTime.Now.AddDays(1),
+                FormaEntregaId = formaEntrega.FormaEntregaId,
+                ComandaMercaderias = new List<ComandaMercaderia>
+                {
+                    new ComandaMercaderia { MercaderiaId = mercaderia.MercaderiaId }
+                }
+            };
+
+            context.Comanda.AddRange(comanda1, comanda2);
+            context.SaveChanges();
+
+            var expectedResponse = comanda2;
+
+            // Act
+            Comanda comanda = await context.Comanda.Include(c => c.ComandaMercaderias)
+                                                   .ThenInclude(c => c.Mercaderia)
+                                                   .ThenInclude(c => c.TipoMercaderia)
+                                                   .Include(c => c.FormaEntrega)
+                                                   .SingleOrDefaultAsync(c => c.ComandaId == expectedResponse.ComandaId);
+            // Assert
+            comanda.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        [Fact]
+        public async Task GetComandaById_ComandaIdDoesNotExist_ShouldReturnNull()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+            using var context = new AppDbContext(options);
+
+            context.Database.EnsureDeleted();
+
+            var tipoMercaderia = new TipoMercaderia
+            {
+                Descripcion = "Entrada"
+            };
+            var mercaderia = new Mercaderia
+            {
+                Nombre = "Deditos de queso",
+                Imagen = "a",
+                Precio = 100,
+                Ingredientes = "a",
+                Preparacion = "a",
+                TipoMercaderiaId = 1
+            };
+            var formaEntrega = new FormaEntrega
+            {
+                Descripcion = "Salon"
+            };
+
+            context.TipoMercaderia.Add(tipoMercaderia);
+            context.Mercaderia.Add(mercaderia);
+            context.FormaEntrega.Add(formaEntrega);
+            context.SaveChanges();
+
+            var comanda1 = new Comanda
+            {
+                ComandaId = Guid.NewGuid(),
+                PrecioTotal = 100,
+                Fecha = DateTime.Now,
+                FormaEntregaId = formaEntrega.FormaEntregaId,
+                ComandaMercaderias = new List<ComandaMercaderia>
+                {
+                    new ComandaMercaderia { MercaderiaId = mercaderia.MercaderiaId }
+                }
+            };
+            var comanda2 = new Comanda
+            {
+                ComandaId = Guid.NewGuid(),
+                PrecioTotal = 200,
+                Fecha = DateTime.Now.AddDays(1),
+                FormaEntregaId = formaEntrega.FormaEntregaId,
+                ComandaMercaderias = new List<ComandaMercaderia>
+                {
+                    new ComandaMercaderia { MercaderiaId = mercaderia.MercaderiaId }
+                }
+            };
+
+            context.Comanda.AddRange(comanda1, comanda2);
+            context.SaveChanges();
+
+            var expectedResponse = comanda2;
+
+            // Act
+            Comanda comanda = await context.Comanda.Include(c => c.ComandaMercaderias)
+                                                   .ThenInclude(c => c.Mercaderia)
+                                                   .ThenInclude(c => c.TipoMercaderia)
+                                                   .Include(c => c.FormaEntrega)
+                                                   .SingleOrDefaultAsync(c => c.ComandaId == Guid.NewGuid());
+            // Assert
+            comanda.Should().BeNull();
+        }
     }
 }
